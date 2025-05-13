0c3f209e0e9c5fc3f2d212d343fb2d28d045a767
Pallavi Chandaka <163713669+Pallavichandaka@users.noreply.github.com>
2025-05-13 22:58:32 +0530

    index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json())
 
 const corsOptions = {
-    origin: "https://www.matricservices.in", 
+    origin: true, 
     // origin:"http://localhost:3000",
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type'],
